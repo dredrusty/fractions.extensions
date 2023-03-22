e40a098beaf5d6811c496ba3cdabcd7369fd8596
Victor Botvinko <rusty.vik@gmail.com>
2023-03-22 23:07:26 +0300

    Correct name

---
+++ Extensions/FractionsExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace Fractions.Extensions;
+
+public static class FractionsExtensions
+{
+    /// <summary>
+    /// Swaps numerator and denominator of the passed fraction and returns a new fraction.
+    /// </summary>
+    /// <param name="fraction"></param>
+    /// <returns>Return</returns>
+    public static Fraction Revert(this Fraction fraction)
+    {
+        int numerator = fraction.Denominator;
+        int denominator = fraction.Numerator;
+
+        return new Fraction(numerator, denominator);        
+    }
+}
